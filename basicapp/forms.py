a2c4be8dff4f0a8a865107f30f39ab7fdec43007
Darzi-Rin <ksw2070039@stu.o-hara.ac.jp>
2022-01-28 11:31:18 +0900

    観測実行完了

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.mail import EmailMessage
-from django.forms import fields
+from django.forms import fields, widgets
+from numpy import require
 from .models import Share
 
 class CreateForm(forms.ModelForm):
@@ -12,3 +13,19 @@ class CreateForm(forms.ModelForm):
             super().__init__(*args, **kwargs)
             for field in self.fields.values():
                 field.widget.attrs['class'] = 'form-control'
+
+
+# 後で作成 with菊池
+# class SelectPortForm(forms.Form):
+#     answers = forms.fields.ChoiceField(
+#         ports = (
+#             "千葉港",
+#             "東京港",
+#             "川崎港",
+#             "横浜港",
+#             "横須賀港",
+#             "木更津港"
+#         ),
+#     required=True,
+#     widget=forms.widgets.Select()
+#     )
--- basicapp/observation_AI.py
+++ basicapp/observation_AI.py
@@ -6,7 +6,7 @@ from keras.models import load_model
 
 def hantei(request):
 
-    model = load_model('c:\\akasio_model.h5')
+    model = load_model('c:/Users/student/Documents/GroupD_Redtide/static/h5/akasio_model.h5')
 
     categories = ["None", "True"]
     #image_pathは判定したい画像のパス
@@ -14,7 +14,7 @@ def hantei(request):
     # img_path = "c:\\2020-01-06-00_00_2020-01-06-23_59_Sentinel-2_L2A_True_color.jpg"
     #img_path = "c:\\2021-01-20-00_00_2021-01-20-23_59_Sentinel-2_L2A_True_colorA.jpg"
     #あり
-    img_path = "c:\\2021-07-22-00_00_2021-07-22-23_59_Sentinel-2_L2A_True_color.jpg"
+    img_path = "c:/Users/student/Documents/GroupD_Redtide/static/images/2021-07-22-00_00_2021-07-22-23_59_Sentinel-2_L2A_True_color.jpg"
     img = image.load_img(img_path, target_size=(60, 60, 3))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
