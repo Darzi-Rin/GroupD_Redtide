244515cfb0ce47fa780c19f799b698a3329ff868
Misakin5656 <misakin.love.c@gmail.com>
2022-01-28 16:38:25 +0900

    未完成

--- basicapp/observation_AI.py
+++ basicapp/observation_AI.py
@@ -15,10 +15,17 @@ def hantei(request):
     categories = ["None", "True"]
     #image_pathは判定したい画像のパス
     #なし
-    # img_path = ("{}/2020-01-06-00_00_2020-01-06-23_59_Sentinel-2_L2A_True_color.jpg".format(AI_IMG))
-    #img_path = ("{}/2021-01-20-00_00_2021-01-20-23_59_Sentinel-2_L2A_True_colorA.jpg".format(AI_IMG))
+    # img_path = "img/2020-01-06-00_00_2020-01-06-23_59_Sentinel-2_L2A_True_color.jpg"
+    #img_path = "img/2021-01-20-00_00_2021-01-20-23_59_Sentinel-2_L2A_True_colorA.jpg"
     #あり
-    img_path = ("{}/2021-07-22-00_00_2021-07-22-23_59_Sentinel-2_L2A_True_color.jpg".format(AI_IMG))
+    # img_path = "img/2021-07-22-00_00_2021-07-22-23_59_Sentinel-2_L2A_True_color.jpg"
+    img_path = "img/2021-07-22-00_00_2021-07-22-23_59_Sentinel-2_L2A_True_color.jpg"
+
+    # img_path = None
+
+    if img_path == None:
+        return render (request,'basic/redtide_observe_ans.html',{'akasio':"画像がありません。"})
+
     img = image.load_img(img_path, target_size=(60, 60, 3))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
@@ -35,7 +42,7 @@ def hantei(request):
         return render (request,'basic/redtide_observe_ans.html',{'akasio':"赤潮はありません。"})
 
     elif features[0,1] == 1:
-        return render (request,'basic/redtide_observe_ans.html',{'akasio':"赤潮が発生しています。"})
+        return render (request,'basic/redtide_observe_ans.html',{'akasio':"赤潮が発生しています。"},{'img':("{}/2021-07-22-00_00_2021-07-22-23_59_Sentinel-2_L2A_True_color.jpg".format(AI_IMG))})
         # print("赤潮が発生しています。")
 
     else:
