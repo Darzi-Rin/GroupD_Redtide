0061e340f09e024880bf2ee1a37cc835bcc0f1f7
KazumaYamaguchi <ksw2070048@stu.o-hara.ac.jp>
2022-01-30 23:13:32 +0000

    models.py途中

--- basicapp/models.py
+++ basicapp/models.py
@@ -1,3 +1,7 @@
+# from accounts.models import CustomUser
 from django.db import models
 
 # Create your models here.
+# class Kyoyu(models.Model):
+
+# user = models.ForeignKey(CustomUser,verbose_name='ユーザー',on_delete=models.PROTECT)
