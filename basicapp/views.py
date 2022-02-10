4cfa14d1a18c9fa1948cee47c147688de8ff2019
Misakin5656 <misakin.love.c@gmail.com>
2022-02-10 13:57:10 +0900

    会員登録完了後ログインページに遷移　追加

--- basicapp/views.py
+++ basicapp/views.py
@@ -60,22 +60,4 @@ class ShareMailView(generic.TemplateView):
 class SignUpView(CreateView):
     template_name = "basic/sign_up.html"
     form_class = UserCreateForm
-    success_url = reverse_lazy("basicapp:sign_up")
-#     def post(self, request, *args, **kwargs):
-#         form = UserCreateForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             #フォームから'username'を読み取る
-#             username = form.cleaned_data.get('username')
-#             #フォームから'password1'を読み取る
-#             password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=password)
-#             login(request, user)
-#             return redirect('/')
-#         return render(request, 'basic/sign_up.html', {'form': form,})
-
-#     def get(self, request, *args, **kwargs):
-#         form = UserCreateForm(request.POST)
-#         return  render(request, 'basic/sign_up.html', {'form': form,})
-
-# create_account = Create_account.as_view()
+    success_url = reverse_lazy("basicapp:login")
